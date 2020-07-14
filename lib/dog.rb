e0efa40f612c9974b23abb628820ab1e43f93cbd
Kiri Um <kirirotha@gmail.com>
2020-07-14 18:59:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,9 +19,17 @@ class Dog
 end
 
 class Person
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
   def job
     @job
   end
+  
   def job=(new_job)
     @job = new_job
   end
